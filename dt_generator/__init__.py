63fcea76779d3f3efe7afea67c2a8318e5bcac0e
nanato12 <admin@nanato12.info>
2021-10-12 03:04:47 +0900

    update: dt_generator

--- dt_generator/__init__.py
+++ dt_generator/__init__.py
@@ -1,9 +1,70 @@
 from datetime import datetime, timedelta
+from math import ceil
 from typing import Generator
+
+__ARGS = {
+    "weeks",
+    "days",
+    "hours",
+    "minutes",
+    "seconds",
+    "milliseconds",
+    "microseconds",
+}
+
+
+def dtrange(
+    start: datetime, end: datetime, **kwargs: int
+) -> Generator[datetime, None, None]:
+    if not (set(kwargs.keys()) <= __ARGS):
+        raise Exception(f"Invalid argments: {set(kwargs.keys()) - __ARGS}")
+
+    tmp = start
+    yield tmp
+
+    for _ in range(ceil((end - start) / timedelta(**kwargs))):
+        tmp = tmp + timedelta(**kwargs)
+        if tmp <= end:
+            yield tmp
+
+
+def weekrange(
+    start: datetime, end: datetime, weeks: int = 1
+) -> Generator[datetime, None, None]:
+    return dtrange(start, end, weeks=weeks)
 
 
 def daterange(
-    start: datetime, end: datetime
+    start: datetime, end: datetime, days: int = 1
 ) -> Generator[datetime, None, None]:
-    for day in range((end - start).days + 1):
-        yield start + timedelta(day)
+    return dtrange(start, end, days=days)
+
+
+def hourrange(
+    start: datetime, end: datetime, hours: int = 1
+) -> Generator[datetime, None, None]:
+    return dtrange(start, end, hours=hours)
+
+
+def minuterange(
+    start: datetime, end: datetime, minutes: int = 1
+) -> Generator[datetime, None, None]:
+    return dtrange(start, end, minutes=minutes)
+
+
+def secondrange(
+    start: datetime, end: datetime, seconds: int = 1
+) -> Generator[datetime, None, None]:
+    return dtrange(start, end, seconds=seconds)
+
+
+def millisecondrange(
+    start: datetime, end: datetime, milliseconds: int = 1
+) -> Generator[datetime, None, None]:
+    return dtrange(start, end, milliseconds=milliseconds)
+
+
+def microsecondrange(
+    start: datetime, end: datetime, microseconds: int = 1
+) -> Generator[datetime, None, None]:
+    return dtrange(start, end, microseconds=microseconds)
