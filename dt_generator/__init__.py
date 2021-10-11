1dfc26c01c2e546f28e23ddd5c7b9220ad6899b0
nanato12 <admin@nanato12.info>
2021-10-12 03:33:35 +0900

    update: dt_generator

--- dt_generator/__init__.py
+++ dt_generator/__init__.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from math import ceil
-from typing import Generator
+from typing import Generator, Optional
 
 __ARGS = {
     "weeks",
@@ -14,16 +14,21 @@ __ARGS = {
 
 
 def dtrange(
-    start: datetime, end: datetime, **kwargs: int
+    start: datetime,
+    end: datetime,
+    td: Optional[timedelta] = None,
+    **kwargs: int,
 ) -> Generator[datetime, None, None]:
     if not (set(kwargs.keys()) <= __ARGS):
         raise Exception(f"Invalid argments: {set(kwargs.keys()) - __ARGS}")
 
+    td = td or timedelta(**kwargs)
+
     tmp = start
     yield tmp
 
-    for _ in range(ceil((end - start) / timedelta(**kwargs))):
-        tmp = tmp + timedelta(**kwargs)
+    for _ in range(ceil((end - start) / td)):
+        tmp = tmp + td
         if tmp <= end:
             yield tmp
 
@@ -68,3 +73,9 @@ def microsecondrange(
     start: datetime, end: datetime, microseconds: int = 1
 ) -> Generator[datetime, None, None]:
     return dtrange(start, end, microseconds=microseconds)
+
+
+def timedeltarange(
+    start: datetime, end: datetime, td: timedelta
+) -> Generator[datetime, None, None]:
+    return dtrange(start, end, td=td)
